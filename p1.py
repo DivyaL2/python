5405a1bc91e3a30cafae12017ebd8dae632f5ef7
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-05-30 04:58:54 +0000

    done the program

---
+++ p1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the number: "))
+b=int(input("Enter the number: "))
+c=int(input("Enter the number: "))
+if a>b and a>c:
+  print(f"{a} is the largest")
+elif b>c:
+  print(f"{b} is the largest")
+else :
+  print(f"{c} is the largest")
+
+
---
+++ p2.py
@@ -0,0 +1,3 @@
+line = "X-DSPAM-Confidence:0.8475"
+num= line.find('0')
+print(num)
